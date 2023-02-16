932f53240bf62df95e87963d1b6692e94503251f
nealgallery1 <125359127+nealgallery1@users.noreply.github.com>
2023-02-16 17:36:54 +0800

    Add files via upload

--- src/js/main.bundle.js
+++ src/js/main.bundle.js
@@ -2,5 +2,18 @@
 /******/ 	"use strict";
 var __webpack_exports__ = {};
 
+var switchNav = document.querySelector('#switchNav');
+var mbNav = document.querySelector('#mbNav');
+var hideNav = document.querySelector('#hideNav');
+console.log(switchNav);
+switchNav.onclick = function () {
+  // mbNav.classList.remove('hidden')
+  // mbNav.classList.remove('w-0')
+  // mbNav.classList.add('!w-2/3')
+  mbNav.style = 'width:80%';
+};
+hideNav.onclick = function () {
+  mbNav.style = 'width:0';
+};
 /******/ })()
 ;
